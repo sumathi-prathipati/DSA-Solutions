afa9809e82cbe30a7b5ab0d3a9e400779f987328
sumathi prathipati <prathipatisumathi5@gmail.com>
2026-08-31 09:02:56 +0530

    Add Medium Java Solution for 189. Rotate Array from LeetCode

--- LeetCode/Java/Medium/189. Rotate Array/Solution.java
+++ LeetCode/Java/Medium/189. Rotate Array/Solution.java
@@ -5,7 +5,7 @@ class Solution {
             for(int i=nums.length-1;i>0;i--){
                 nums[i] = nums[i-1];
             }
-            nums[0]=last;
+            // nums[0]=last;
         }
     }
 }
