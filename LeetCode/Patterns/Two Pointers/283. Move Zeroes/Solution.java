7380f82124673e488b8e10f32f807b0868734b7d
sumathi prathipati <prathipatisumathi5@gmail.com>
2026-09-02 09:55:04 +0530

    Add Easy Java Solution for 283. Move Zeroes from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        int n = nums.length;
+        for( int i =0; i<n ; i++){
+            if (nums[i]!=0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+        
+    }
+}
